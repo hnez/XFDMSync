f2edd129353f9c0409ef0aa711a413b0ed28a9f0
Leonard Göhrs <leonard@goehrs.eu>
2017-11-05 13:18:57 +0100

    Changed the way the fqsweep correlator works

--- python/fqsweep_corr.py
+++ python/fqsweep_corr.py
@@ -19,6 +19,7 @@
 # Boston, MA 02110-1301, USA.
 
 from gnuradio import gr, blocks
+import numpy as np
 
 class fqsweep_corr(gr.hier_block2):
     """
@@ -31,32 +32,51 @@ class fqsweep_corr(gr.hier_block2):
             gr.io_signature(2, 2, gr.sizeof_gr_complex)
         )
 
-        half_length= int(preamble_length)/2
+        despread= self.gen_despread(
+            preamble_length,
+            np.pi * 3.0 / 4.0
+        )
 
         # Blocks
-        self.equiv_delay= blocks.delay(gr.sizeof_gr_complex, preamble_length)
+        self.equiv_delay= blocks.delay(gr.sizeof_gr_complex, preamble_length + 2)
 
-        self.fqcal_delay= blocks.delay(gr.sizeof_gr_complex, 1)
-        self.fqcal_cmul= blocks.multiply_conjugate_cc(1)
+        self.despread_src= blocks.vector_source_c(despread, True, 1, [])
+        self.despread_mul= blocks.multiply_conjugate_cc(1)
 
-        self.fqcmp_delay= blocks.delay(gr.sizeof_gr_complex, half_length)
-        self.fqcmp_mul= blocks.multiply_vcc(1)
+        self.deriv_0_delay= blocks.delay(gr.sizeof_gr_complex, 1)
+        self.deriv_0_mul= blocks.multiply_conjugate_cc(1)
 
-        self.avg= blocks.moving_average_cc(half_length, 1, 4096)
+        self.deriv_1_delay= blocks.delay(gr.sizeof_gr_complex, 1)
+        self.deriv_1_mul= blocks.multiply_conjugate_cc(1)
+
+        self.avg= blocks.moving_average_cc(preamble_length, 1.0/preamble_length, 2**16)
 
         # Connections
-        self.connect((self, 0), (self.fqcal_delay, 0))
-        self.connect((self, 0), (self.fqcal_cmul, 0))
+
+        # pass output
         self.connect((self, 0), (self.equiv_delay, 0))
-
-        self.connect((self.fqcal_delay, 0), (self.fqcal_cmul, 1))
-
-        self.connect((self.fqcal_cmul, 0), (self.fqcmp_delay, 0))
-        self.connect((self.fqcal_cmul, 0), (self.fqcmp_mul, 0))
-
-        self.connect((self.fqcmp_delay, 0), (self.fqcmp_mul, 1))
-
-        self.connect((self.fqcmp_mul, 0), (self.avg, 0))
-
         self.connect((self.equiv_delay, 0), (self, 0))
-        self.connect((self.avg, 0), (self, 1))
+
+        # despread
+        self.connect((self, 0), (self.despread_mul, 0))
+        self.connect((self.despread_src, 0), (self.despread_mul, 1))
+
+        # first phase derivate (frequency)
+        self.connect((self.despread_mul, 0), (self.deriv_0_mul, 0))
+        self.connect((self.despread_mul, 0), (self.deriv_0_delay, 0))
+        self.connect((self.deriv_0_delay, 0), (self.deriv_0_mul, 1))
+
+        # second phase derivate (change of frequency)
+        self.connect((self.deriv_0_mul, 0), (self.deriv_1_mul, 0))
+        self.connect((self.deriv_0_mul, 0), (self.deriv_1_delay, 0))
+        self.connect((self.deriv_1_delay, 0), (self.deriv_1_mul, 1))
+
+        # average
+        self.connect((self.deriv_1_mul, 0), (self, 1))
+
+    def gen_despread(self, preamble_length, span):
+        freqs= np.linspace(-span, span, preamble_length)
+        phases= np.add.accumulate(freqs)
+        samples= np.exp(1j * phases)
+
+        return samples
